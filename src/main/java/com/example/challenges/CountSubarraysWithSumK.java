36f58fecd87727910df0bd5b07d5c85cd4ff9f07
default <antonio@v4lle.tech>
2025-10-15 19:09:47 +0000

    refactor: uso de hashmap para control

--- src/main/java/com/example/challenges/CountSubarraysWithSumK.java
+++ src/main/java/com/example/challenges/CountSubarraysWithSumK.java
@@ -1,12 +1,14 @@
 package com.example.challenges;
 
+import java.util.HashMap;
+
 public class CountSubarraysWithSumK {
 
     public static void main(String[] args) {
-        int[] numsA = { 1, 1, 1 };
-        int k = 1;
+        int[] numsA = { 1, 2, 3 }; // 2
+        int k = 3;
         System.out.println(subarraySumPrefixSum(numsA, k));
-        int[] numsB = { 1, 2, 3 };
+        int[] numsB = { 1, 0, 1, 1, 2, 3 }; // 3
         k = 3;
         System.out.println(subarraySumPrefixSum(numsB, k));
     }
@@ -17,15 +19,19 @@ public class CountSubarraysWithSumK {
         }
 
         int count = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == k) {
-                count++;
+        int sum = 0;
+        HashMap<Integer, Integer> prefixSumCount = new HashMap<>();
+        prefixSumCount.put(0, 1); // Para subarrays que empiezan desde el inicio
+
+        for (int num : nums) {
+            sum += num;
+            // Verificar si existe un prefix sum anterior que sea sum - k
+            if (prefixSumCount.containsKey(sum - k)) {
+                count += prefixSumCount.get(sum - k);
             }
-            if (i > 0) {
-                if (k == (nums[i - 1] + nums[i])) {
-                    count++;
-                }
-            }
+            // Actualizar la frecuencia del prefix sum actual
+            prefixSumCount.put(sum, prefixSumCount.getOrDefault(sum, 0) + 1);
+            System.out.println(prefixSumCount.toString());
         }
 
         return count;
